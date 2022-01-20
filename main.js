f38535316bad2966d0c185f63d9e0548bd4c37b2
marco maione <maionemarco14@gmail.com>
2022-01-20 16:17:40 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -13,13 +13,17 @@ console.log(eta);
 // va applicato uno sconto del 40% per gli over 65 //
 
 if (eta < 18 ) {
-    let discount = price / 20 * 100 - price;
+    let discount = price / 100 * 20 - price;
     document.getElementById("finalPrice").innerHTML = discount + "euro";
     price.toFixed(2)
 }
 
 else if ( eta > 65) {
-    let senior = price / 40 * 100 - price;
+    let senior = price / 100 * 40 - price;
     document.getElementById("finalPrice").innerHTML = senior + "euro";
     price.toFixed(2)
+}
+
+else ( eta > 18 && eta < 65) {
+    document.getElementById("finalPrice").innerHTML = price + "euro";
 }
