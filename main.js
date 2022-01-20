039091f076ea67b276d980354b8c421c61be90ed
marco maione <maionemarco14@gmail.com>
2022-01-20 18:53:46 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 //numero di km che vuole percorrere//
 
 let km = prompt("inserire km da percorrere");
-console.log(km);
 
 let price = km * 0.21;
+
 //età passeggero// 
 
-let eta = prompt( "inserire la propria eta")
-console.log(eta);
+let eta = prompt( "inserire la propria eta");
 
 // va applicato uno sconto del 20% per i minorenni //
 // va applicato uno sconto del 40% per gli over 65 //
@@ -17,6 +16,7 @@ if (eta < 18 ) {
     let finalPrice = price - discount;
     document.getElementById("finalPrice").innerHTML = finalPrice + "euro";
     price.toFixed(2);
+    console.log("sconto giovani");
 }
 
 else if ( eta > 65) {
@@ -24,9 +24,11 @@ else if ( eta > 65) {
     let finalPrice = price - senior;
     document.getElementById("finalPrice").innerHTML = senior + "euro";
     price.toFixed(2);
+    console.log("sconto anziani");
 }
 
-else {  eta > 18 && eta < 65 
+else {
     document.getElementById("finalPrice").innerHTML = price + "euro";
+    console.log("prezzo pieno");
 }
 
